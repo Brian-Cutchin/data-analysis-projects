6d1653580903f5586b7376e09438f4f60709a4ee
Brian Cutchin <bccutchi@gmail.com>
2024-08-28 00:06:49 -0500

    completed studio

--- collections/studio/list-and-string-conversion.py
+++ collections/studio/list-and-string-conversion.py
@@ -6,18 +6,61 @@ proto_list4 = "Comma-spaces, might, require, typing, caution"
 strings = [proto_list1, proto_list2, proto_list3, proto_list4]
 
 # a) Use the 'in' method to check to see if the words in each string are separated by commas (,), semicolons (;) or just spaces.
+def check_separator(string):
+    if ',' in string:
+        return "comma"
+    elif ';' in string:
+        return "semicolon"
+    elif ' ' in string:
+        return "space"
+    else:
+        return "unknown"
 
+
+for i, s in enumerate(strings):
+    separator = check_separator(s)
+    print(f"String {i+1} is separated by {separator}.")
 
 # b) If the string uses commas to separate the words, split it into an array, reverse the entries, and then join the array into a new comma separated string.
+def process_comma_separated(string):
+    items = string.split(',')
+    items.reverse()
+    return ','.join(items)
 
 
 
 # c) If the string uses semicolons to separate the words, split it into an array, alphabetize the entries, and then join the array into a new comma separated string.
+def process_semicolon_separated(string):
+    items = string.split(';')
+    items.sort()
+    return ','.join(items)
 
 
 
 # d) If the string uses spaces to separate the words, split it into an array, reverse alphabetize the entries, and then join the array into a new space separated string.
-
+def process_space_separated(string):
+    items = string.split(' ')
+    items.sort(reverse=True)
+    return ' '.join(items)
 
 
 # e) If the string uses ‘comma spaces’ to separate the list, modify your code to produce the same result as part “b”, making sure that the extra spaces are NOT part of the final string.
+def process_comma_space_separated(string):
+    items = [item.strip() for item in string.split(',')]
+    items.reverse()
+    return ','.join(items)
+
+for i, s in enumerate(strings):
+    separator = check_separator(s)
+    if separator == "comma":
+        result = process_comma_separated(s)
+    elif separator == "semicolon":
+        result = process_semicolon_separated(s)
+    elif separator == "space":
+        result = process_space_separated(s)
+    elif separator == "comma space":
+        result = process_comma_space_separated(s)
+    else:
+        result = "Unknown separator"
+    
+    print(f"Processed String {i+1}: {result}")
--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,14 +2,27 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-
+new_string = my_string[3:] + my_string[0:3]
+print(new_string)
 
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-
+print(f'My old word is {my_string} but my new word is {new_string}')
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+x = int(input('Enter a number to relocate:'))
+new_string = my_string[x:] + my_string[0:x]
+print(new_string)
 
 # c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+x = int(input('Enter a number to relocate:'))
+
+if len(my_string) < x:
+    print("Error: Number of letters to relocate is too large.")
+    x = 3
+
+new_string = my_string[x:] + my_string[0:x]
+print(f'My old word is {my_string} but my new word is {new_string}')
